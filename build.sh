84e10c931418c50570f20a3c9ffeefbd6fe24788
my5t3ry <sash@mischkonsum.org>
2020-10-03 10:59:25 +0200

    gitignore

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 g++ -Wall -D__LINUX_ALSA__ -o client client.cpp RtMidi.cpp -lasound -lpthread
 g++ -o server server.cpp -lpthread
-
-
--- rtmidi.cpp
+++ rtmidi.cpp
@@ -4,8 +4,6 @@
 #include <string>
 #include <string>
 
-
-
 #include <unistd.h>
 #include "libsocket/exception.hpp"
 #include "libsocket/inetclientstream.hpp"
@@ -14,52 +12,55 @@
  * Sends and receives messages via connected UDP sockets
  */
 
-void mycallback( double deltatime, std::vector< unsigned char > *message, void *userData )
+void mycallback (double deltatime, std::vector<unsigned char> *message, void *userData)
 {
-  unsigned int nBytes = message->size();
-  for ( unsigned int i=0; i<nBytes; i++ )
-    std::cout << "Byte " << i << " = " << (int)message->at(i) << ", ";
-  if ( nBytes > 0 )
+  unsigned int nBytes = message->size ();
+  for (unsigned int i = 0; i < nBytes; i++)
+    std::cout << "Byte " << i << " = " << (int) message->at (i) << ", ";
+  if (nBytes > 0)
     std::cout << "stamp = " << deltatime << std::endl;
 }
-int main()
+int main ()
 {
-  RtMidiIn *midiin = new RtMidiIn();
+  RtMidiIn *midiin = new RtMidiIn ();
   // Check available ports.
-  unsigned int nPorts = midiin->getPortCount();
-  if ( nPorts == 0 ) {
-    std::cout << "No ports available!\n";
-  }
-  midiin->openVirtualPort("test");
+  unsigned int nPorts = midiin->getPortCount ();
+  if (nPorts == 0)
+    {
+      std::cout << "No ports available!\n";
+    }
+  midiin->openVirtualPort ("test");
   // Set our callback function.  This should be done immediately after
   // opening the port to avoid having incoming messages written to the
   // queue.
-  midiin->setCallback( &mycallback );
+  midiin->setCallback (&mycallback);
   // Don't ignore sysex, timing, or active sensing messages.
-  midiin->ignoreTypes( false, false, false );
-      using std::string;
+  midiin->ignoreTypes (false, false, false);
+  using std::string;
 
-       using libsocket::inet_stream;
+  using libsocket::inet_stream;
 
-       string host = "127.0.0.1";
-       string port = "1234";
-       string answer;
+  string host = "127.0.0.1";
+  string port = "1234";
+  string answer;
 
-       answer.resize(32);
+  answer.resize (32);
 
-       try {
-           libsocket::inet_stream sock(host, port, LIBSOCKET_IPv4);
+  try
+    {
+      libsocket::inet_stream sock (host, port, LIBSOCKET_IPv4);
 
-           sock >> answer;
+      sock >> answer;
 
-           std::cout << answer;
+      std::cout << answer;
 
-           sock << "Hello back!\n";
+      sock << "Hello back!\n";
 
-           // sock is closed here automatically!
-       } catch (const libsocket::socket_exception& exc) {
-           std::cerr << exc.mesg;
-       }
-
+      // sock is closed here automatically!
+    }
+  catch (const libsocket::socket_exception &exc)
+    {
+      std::cerr << exc.mesg;
+    }
 
 }
