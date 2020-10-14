81a1ff9505c0d9363e6b5116ee5b5f2997e2dde3
my5t3ry <sash@mischkonsum.org>
2020-10-14 07:46:31 +0200

    static build

--- server.cpp
+++ server.cpp
@@ -181,13 +181,11 @@ void midi_clock(int clock_rate, chat_room *room, long midi_buffer = 500) {
 }
 void send_start_message(int clock_rate, chat_room *room, long midi_buffer) {
   nlohmann::json message;
-
   message["bytes"][0] = MIDI_CMD_COMMON_START;
   message["meta"]["uuid"] = room->GetUuid();
   message["meta"]["clock_rate"] = clock_rate;
   message["meta"]["exec_timestamp"] = get_posix_timestamp(midi_buffer);
   LOG(DEBUG) << "MIDI start";
-
   room->deliver(message);
 }
 
